1d24c31ec48e3c5dc367e1f617cfc69c6de6a57d
Fio <hello@f-io.dev>
2025-09-06 11:19:56 +0200

    renderer(webgl2): switch texture filtering to LINEAR

--- src/renderer/src/components/worker/render/WebGL2Renderer.ts
+++ src/renderer/src/components/worker/render/WebGL2Renderer.ts
@@ -53,8 +53,8 @@ export class WebGL2Renderer implements FrameRenderer {
 
     const texture = gl.createTexture()
     gl.bindTexture(gl.TEXTURE_2D, texture)
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
 
